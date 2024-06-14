a7fe1c6a1cf651f3fd7f06d442b2d21e2b582542
MMiah1 <mohsinmiah@hollandandbarrett.com>
2024-06-14 15:23:49 +0100

    modify unique Ids reflecting date

--- services/generateUniqueId.js
+++ services/generateUniqueId.js
@@ -4,14 +4,19 @@ export const generateUniqueGameId = (existingGames) => {
   // Get today's date in the desired format
   const today = moment().format("DD-MM-YYYY");
 
+  // Filter out games for today
+  const todayGames = existingGames.filter((game) =>
+    game.gameId.startsWith(today)
+  );
+
   // Initialize game number
-  let gameNumber = 1;
+  let gameNumber = todayGames.length + 1;
   let gameId;
 
   // Loop to find a unique game ID
   while (true) {
     gameId = `${today}-game-${gameNumber}`;
-    const gameExists = existingGames.some((game) => game.gameId === gameId);
+    const gameExists = todayGames.some((game) => game.gameId === gameId);
 
     if (!gameExists) {
       break;
