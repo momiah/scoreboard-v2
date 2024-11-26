48f54ae0e6397a00560a1d1aa93d09b615a7a6b9
MMiah1 <mohsinmiah@hollandandbarrett.com>
2024-11-26 17:18:39 +0000

    setup navigation structure

---
+++ screens/Notifications.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Notifications = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>Profile</Text>
+      <Text>This is the profile screen.</Text>
+    </View>
+  );
+};
+
+export default Notifications;
---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home/Home";
+import Profile from "./Profile";
+import Notifications from "./Notifications";
+
+export { Home, Profile, Notifications };
