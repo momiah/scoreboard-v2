816023dac4ae957e30e7946361393b5ab35c67da
MMiah1 <mohsinmiah@hollandandbarrett.com>
2024-12-06 19:20:10 +0000

    alignment and styling

---
+++ components/Tag.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Text } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import styled from "styled-components/native";
+
+const Tag = ({ name, color = "#2F2F30", icon }) => {
+  return (
+    <TagContainer backgroundColor={color}>
+      {icon && <TagIcon name={icon} size={24} color="white" />}
+      <TagText>{name.toUpperCase()}</TagText>
+    </TagContainer>
+  );
+};
+const TagContainer = styled.View(({ backgroundColor }) => ({
+  flexDirection: "row",
+  alignItems: "center",
+  padding: 6,
+  borderRadius: 6,
+  margin: 4,
+  backgroundColor,
+  alignSelf: "flex-start",
+}));
+
+const TagIcon = styled(Ionicons)({
+  marginRight: 8,
+});
+
+const TagText = styled.Text({
+  color: "white",
+  fontSize: 10,
+});
+
+export default Tag;
