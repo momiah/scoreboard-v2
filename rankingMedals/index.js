fe9611b30ce27a3dce3d97173cf77e09526da823
MMiah1 <mohsinmiah@hollandandbarrett.com>
2025-06-08 10:55:00 +0100

    update streak medals

--- rankingMedals/index.js
+++ rankingMedals/index.js
@@ -1,8 +1,12 @@
 const gameMedals = {
-  win_streak_3: require("./WinStreak_3.png"),
-  win_streak_5: require("./WinStreak_5.png"),
-  win_streak_7: require("./WinStreak_7.png"),
-  demon_win: require("./Demon_Win.png"),
+  win_streak_3: require("./winStreak3.png"),
+  win_streak_5: require("./winStreak5.png"),
+  win_streak_7: require("./winStreak7.png"),
+  // win_streak_3: require("./WinStreak_3.png"),
+  // win_streak_5: require("./WinStreak_5.png"),
+  // win_streak_7: require("./WinStreak_7.png"),
+  // demon_win: require("./Demon_Win.png"),
+  demon_win: require("./assassin.png"),
 };
 
 export default gameMedals;
