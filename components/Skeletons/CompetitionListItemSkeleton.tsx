aaedba0cb610707adb84e4a8f1b2e41295c31deb
Mohsin Miah <mohsinmiah@lizahussainmiahs-MacBook-Pro.local>
2026-06-27 11:44:42 +0100

    update skeleton to add comp avatar

--- components/Skeletons/CompetitionListItemSkeleton.tsx
+++ components/Skeletons/CompetitionListItemSkeleton.tsx
@@ -6,38 +6,56 @@ import { TextSkeleton } from "./SkeletonComponents";
 const { width: screenWidth } = Dimensions.get("window");
 const isSmallScreen = screenWidth < 400;
 
+const AVATAR_SIZE = isSmallScreen ? 44 : 52;
+
 const CompetitionListItemSkeleton = memo(() => (
   <ListItemContainer activeOpacity={1} disabled>
-    <TopRow>
-      <CompetitionInfo>
-        <TextSkeleton show height={isSmallScreen ? 14 : 16} width={160} />
-        <View style={{ marginTop: 6 }}>
-          <TextSkeleton show height={isSmallScreen ? 11 : 12} width={90} />
-        </View>
-      </CompetitionInfo>
+    <RowContainer>
+      <LeftColumn>
+        <AvatarSkeleton />
+      </LeftColumn>
 
-      <StatsRow>
-        <StatBlock>
-          <TextSkeleton show height={isSmallScreen ? 10 : 11} width={28} />
-          <View style={{ marginTop: 4 }}>
-            <TextSkeleton show height={isSmallScreen ? 20 : 24} width={24} />
-          </View>
-        </StatBlock>
-        <StatBlock>
-          <TextSkeleton show height={isSmallScreen ? 10 : 11} width={28} />
-          <View style={{ marginTop: 4 }}>
-            <TextSkeleton show height={isSmallScreen ? 20 : 24} width={24} />
-          </View>
-        </StatBlock>
-      </StatsRow>
-    </TopRow>
+      <ContentColumn>
+        <TopRow>
+          <CompetitionInfo>
+            <TextSkeleton show height={isSmallScreen ? 14 : 16} width={160} />
+            <View style={{ marginTop: 6 }}>
+              <TextSkeleton show height={isSmallScreen ? 11 : 12} width={90} />
+            </View>
+          </CompetitionInfo>
 
-    <MetaRow>
-      <MetaLeft>
-        <TextSkeleton show height={12} width={140} />
-      </MetaLeft>
-      <TextSkeleton show height={12} width={90} />
-    </MetaRow>
+          <StatsRow>
+            <StatBlock>
+              <TextSkeleton show height={isSmallScreen ? 10 : 11} width={28} />
+              <View style={{ marginTop: 4 }}>
+                <TextSkeleton
+                  show
+                  height={isSmallScreen ? 20 : 24}
+                  width={24}
+                />
+              </View>
+            </StatBlock>
+            <StatBlock>
+              <TextSkeleton show height={isSmallScreen ? 10 : 11} width={28} />
+              <View style={{ marginTop: 4 }}>
+                <TextSkeleton
+                  show
+                  height={isSmallScreen ? 20 : 24}
+                  width={24}
+                />
+              </View>
+            </StatBlock>
+          </StatsRow>
+        </TopRow>
+
+        <MetaRow>
+          <MetaLeft>
+            <TextSkeleton show height={12} width={140} />
+          </MetaLeft>
+          <TextSkeleton show height={12} width={90} />
+        </MetaRow>
+      </ContentColumn>
+    </RowContainer>
   </ListItemContainer>
 ));
 
@@ -49,6 +67,32 @@ const ListItemContainer = styled.TouchableOpacity({
   backgroundColor: "rgb(3, 16, 31)",
   borderTopWidth: 1,
   borderColor: "rgb(9, 33, 62)",
+});
+
+const RowContainer = styled.View({
+  flexDirection: "row",
+  alignItems: "center",
+  width: "100%",
+  gap: 12,
+});
+
+const LeftColumn = styled.View({
+  width: AVATAR_SIZE,
+  alignItems: "center",
+  justifyContent: "center",
+  gap: 8,
+});
+
+const AvatarSkeleton = styled.View({
+  width: AVATAR_SIZE,
+  height: AVATAR_SIZE,
+  borderRadius: AVATAR_SIZE / 2,
+  backgroundColor: "rgb(9, 33, 62)",
+});
+
+const ContentColumn = styled.View({
+  flex: 1,
+  flexDirection: "column",
 });
 
 const TopRow = styled.View({
